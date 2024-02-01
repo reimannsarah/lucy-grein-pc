2b8d6b841368d735d1114bdf573ca5b8b39424e9
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-01 13:02:19 -0800

    Add imageUrls.ts file with image URLs

---
+++ src/assets/imgs/imageUrls.ts
@@ -0,0 +1,7 @@
+export const imageUrls = {
+  beach: "https://lucygreinpc.blob.core.windows.net/photos/beach.png",
+  fall_creek: "https://lucygreinpc.blob.core.windows.net/photos/lucy-fall-creek.png",
+  hero: "https://lucygreinpc.blob.core.windows.net/photos/LucyBubba.png",
+  la: "https://lucygreinpc.blob.core.windows.net/photos/LucyLA.png",
+  logo: "https://lucygreinpc.blob.core.windows.net/photos/LucyLogo.png"
+}
