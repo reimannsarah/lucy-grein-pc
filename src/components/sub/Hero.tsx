4e3f94605ed7861400b9915cc6b3f37ad4ae4125
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-01 16:21:23 -0800

    Add Hero component

---
+++ src/components/sub/Hero.tsx
@@ -0,0 +1,11 @@
+import { imageUrls } from "../../assets/imgs/imageUrls"
+
+const Hero = () => {
+  return (
+    <div>
+      <img src={imageUrls.hero} alt="" className="w-full"/>
+    </div>
+  )
+}
+
+export default Hero
