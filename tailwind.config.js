3f456020b3b2c5830bd243a0035f22041cec7543
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-01 16:21:01 -0800

    Update tailwind.config.js with new content and extended theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "#fbe5d4",
+        green: "#eaefd7",
+        purple: "#e7e6fc",
+        pink: "#f9e8fb",
+        lightBlue: "#d3eaff",
+        darkBlue: "#bed4f8",
+        yellow: "#eac3359f",
+        coolPurple: "#646cffaa",
+        background: "#fffafa"
+      }
+    },
   },
   plugins: [],
 }
