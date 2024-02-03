622bfeb9d03a1d38eb1466e61604036890d6267a
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-02 23:20:26 -0800

    add shadow to hero photo

--- src/components/sub/Hero.tsx
+++ src/components/sub/Hero.tsx
@@ -3,7 +3,7 @@ import { imageUrls } from "../../assets/imgs/imageUrls"
 const Hero = () => {
   return (
     <div>
-      <img src={imageUrls.hero} alt="" className="w-full"/>
+      <img src={imageUrls.hero} alt="" className="w-full shadow-md rounded-lg"/>
     </div>
   )
 }
