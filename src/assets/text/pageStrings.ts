af77e560a9186d68b59491487691caa9fe82185e
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-02 23:20:12 -0800

    add pageStrings content

--- src/assets/text/pageStrings.ts
+++ src/assets/text/pageStrings.ts
@@ -1,3 +1,19 @@
 export const pageStrings = { 
-  
+  hero: {
+    title: "Hi, I'm Lucy",
+  },
+  boxes: {
+    box1: {
+      top: "Working with families is my passion",
+      bottom: "Bio"
+    },
+    box2: {
+      top: "I believe parents deserve support and collaboration",
+      bottom: "Programs"
+    },
+    box3: {
+      top: "Intuition is a comlex system of values, perception, and memory",
+      bottom: "Intuition Theory"
+    }
+  }
 }
