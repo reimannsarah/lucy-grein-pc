db380e51f8508d4b3953990c46f9b19a9f8b6ef3
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-02 23:20:38 -0800

    Refactor grid layout in InfoBoxes component

--- src/components/sub/InfoBoxes.tsx
+++ src/components/sub/InfoBoxes.tsx
@@ -1,14 +1,25 @@
+import { pageStrings } from "../../assets/text/pageStrings";
+
 const InfoBoxes = () => {
   return (
     <div className="">
-      <div className="grid grid-cols-4 grid-rows-6 gap-4">
-        <div className="w-full h-[250px] border-2 border-black col-span-2 row-span-2"></div>
-        <div className="w-full h-[250px] border-2 border-black col-span-2 row-span-2 col-start-3"></div>
-        <div className="w-full h-[250px] border-2 border-black col-span-2 row-span-2 row-start-3"></div>
-        <div className="w-full h-[250px] border-2 border-black col-span-2 row-span-2 col-start-3 row-start-3"></div>
-        <div className="w-full h-[250px] border-2 border-black col-span-2 row-span-2 row-start-5"></div>
-        <div className="w-full h-[250px] border-2 border-black col-span-2 row-span-2 col-start-3 row-start-5"></div>
-      </div>
+      <div className="grid" style={{ gridTemplateColumns: '15px repeat(2, 125px) repeat(3, 15px) repeat(2, 125px) 15px', gridTemplateRows: '15px repeat(2, 125px) repeat(2, 15px) repeat(2, 125px) 15px repeat(2, 125px) 15px', gridColumnGap: 0, gridRowGap: 0 }}>
+      <div id="1" className="flex justify-center items-center rounded-lg col-span-3 row-span-3 bg-darkBlue font-header" style={{ gridArea: '2 / 2 / 5 / 5' }}>{pageStrings.boxes.box1.bottom}</div>
+
+      <div id="2" className="flex justify-center items-center rounded-lg col-span-3 row-span-3 bg-lightBlue font-body hover:-z-50" style={{ gridArea: '1 / 1 / 4 / 4' }}>{pageStrings.boxes.box1.top}</div>
+
+      <div id="3" className="flex justify-center items-center rounded-lg col-span-3 row-span-3 bg-darkBlue font-header" style={{ gridArea: '6 / 7 / 9 / 10' }}>{pageStrings.boxes.box2.bottom}</div>
+
+      <div id="4" className="flex justify-center items-center rounded-lg col-span-3 row-span-3 bg-lightBlue font-body hover:-z-50" style={{ gridArea: '5 / 6 / 8 / 9' }}>{pageStrings.boxes.box2.top}</div>
+
+      <div id="5" className="flex justify-center items-center rounded-lg col-span-3 row-span-3 bg-darkBlue font-header" style={{ gridArea: '9 / 2 / 12 / 5' }}>{pageStrings.boxes.box3.bottom}</div>
+
+      <div id="6" className="flex justify-center items-center rounded-lg col-span-3 row-span-3 bg-lightBlue font-body hover:-z-50" style={{ gridArea: '8 / 1 / 11 / 4' }}>{pageStrings.boxes.box3.top}</div>
+
+      <div id="7" className="col-span-5 row-span-3" style={{ gridArea: '5 / 1 / 8 / 6' }}></div>
+      <div id="8" className="col-span-5 row-span-4" style={{ gridArea: '1 / 5 / 5 / 10' }}></div>
+      <div id="9" className="col-span-5 row-span-3" style={{ gridArea: '9 / 5 / 12 / 10' }}></div>
+    </div>
     </div>
   );
 };
