59cf7824b411c95b88c8d366038c0f98b89098d7
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-08 23:30:48 -0800

    WIP

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -1,15 +1,11 @@
-import Hero from "../sub/Hero";
-import InfoBoxes from "../sub/InfoBoxes";
+// import Hero from "../sub/Hero";
+// import InfoBoxes from "../sub/InfoBoxes";
+import SVG from "../sub/svg";
 
 const Home = () => {
   return (
-    <div className="flex flex-row mt-52 gap-5 items-center">
-      <div className="w-1/2">
-        <Hero />
-      </div>
-      <div className="w-1/2">
-        <InfoBoxes />
-      </div>
+    <div>
+      <SVG />
     </div>
   );
 };
