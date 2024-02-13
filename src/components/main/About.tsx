aa732e05f0fa57ea4975a0b6645416e69a31f8ab
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-12 17:48:49 -0800

    WIP

--- src/components/main/About.tsx
+++ src/components/main/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className="bg-background my-56 mx-44 p-20 rounded-lg shadow-lg font-body text-left">
       <div>
-        <div className="flex flex-row">
+        <div className="flex flex-row mb-14">
           <div className="w-1/2">
             <h1 className="text-4xl mb-4 text-left text-orange">{pageStrings.about.introHeader}</h1>
             <p className="text-lg leading-10">{pageStrings.about.introP1}</p>
@@ -23,12 +23,12 @@ const About = () => {
         </div>
         <div className="clearfix"></div>
         <div>
-          <h1>{pageStrings.about.journeyHeader}</h1>
-          <p>{pageStrings.about.journeyP1}</p>
-          <p>{pageStrings.about.journeyP2}</p>
-          <p>{pageStrings.about.journeyP3}</p>
-          <p>{pageStrings.about.journeyP4}</p>
-          <p>{pageStrings.about.journeyP5}</p>
+          <h1 className="text-4xl text-orange text-right mb-4">{pageStrings.about.journeyHeader}</h1>
+          <p className="text-lg leading-10">{pageStrings.about.journeyP1}</p>
+          <p className="text-lg leading-10">{pageStrings.about.journeyP2}</p>
+          <p className="text-lg leading-10">{pageStrings.about.journeyP3}</p>
+          <p className="text-lg leading-10">{pageStrings.about.journeyP4}</p>
+          <p className="text-lg leading-10">{pageStrings.about.journeyP5}</p>
         </div>
       </div>
     </div>
