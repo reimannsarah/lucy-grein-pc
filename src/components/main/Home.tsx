c91bc14b76b28b4f2c15102e76f81f019c083d8a
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-12 17:45:55 -0800

    Remove unused imports and commented code

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -1,13 +1,9 @@
-// import Hero from "../sub/Hero";
-// import InfoBoxes from "../sub/InfoBoxes";
-// import Background from "../sub/Background";
 import Logo from "../sub/Logo";
 
 const Home = () => {
   return (
     <div>
       <Logo />
-      {/* <Background /> */}
     </div>
   );
 };
