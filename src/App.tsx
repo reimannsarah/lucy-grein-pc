55610605c426836568b5632416cabff33e93518f
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-12 17:45:04 -0800

    render about component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/main/Home";
 import Layout from "./components/main/Layout";
+import About from "./components/main/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/*" element={<Layout />}>
           <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </Router>
