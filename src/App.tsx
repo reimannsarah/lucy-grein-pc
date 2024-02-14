dda6828ec14ce5be06c6662cae9d4706e0bb5d63
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-14 13:20:28 -0800

    add landing page route

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/main/Home";
 import Layout from "./components/main/Layout";
 import About from "./components/main/About";
+import Landing from "./components/main/Landing";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Landing />}/>
         <Route path="/*" element={<Layout />}>
           <Route path="home" element={<Home />} />
           <Route path="about" element={<About />} />
