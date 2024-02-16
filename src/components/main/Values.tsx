5a7940a83395e3689e16543e9f07915d3e57bd67
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-16 14:00:20 -0800

    Refactor Values component and add Accordion

--- src/components/main/Values.tsx
+++ src/components/main/Values.tsx
@@ -1,18 +1,49 @@
-import { imageUrls } from "../../assets/imgs/imageUrls"
-import { pageStrings } from "../../assets/text/pageStrings"
+import { imageUrls } from "../../assets/imgs/imageUrls";
+import { pageStrings } from "../../assets/text/pageStrings";
+import Accordion from "../sub/Accordion";
 
 const Values = () => {
   return (
     <div className="bg-background my-56 mx-44 p-20 rounded-lg shadow-lg font-body">
-      <div>
+      <div className="flex flex-row items-center justify-evenly">
         <div>
-          <h1>{pageStrings.values.title}</h1>
-          <p>{pageStrings.values.p1}</p>
+          <h1 className="text-orange text-4xl text-left mb-10">{pageStrings.values.title}</h1>
+          <p className="leading-10 w-4/5 text-left text-xl text-gray">{pageStrings.values.p1}</p>
         </div>
-        <img src={imageUrls.beach} alt="" />
+        <img src={imageUrls.beach} alt="" className="w-2/5 rounded-full border-[20px] border-pink"/>
+      </div>
+      <div className="flex flex-row gap-10 mt-10">
+        <div>
+          <h1 className="text-3xl text-coolPurple">{pageStrings.values.values_title}</h1>
+          <div className="bg-purple p-10 m-10 shadow-lg">
+            {Object.entries(pageStrings.values.core_values).map(
+              ([key, value], index) => {
+                return (
+                  <div key={index}>
+                    <Accordion title={key} description={value} />
+                  </div>
+                );
+              }
+            )}
+          </div>
+        </div>
+        <div>
+          <h1 className="text-3xl text-coolPurple">{pageStrings.values.philisophies_title}</h1>
+          <div className="bg-purple p-10 m-10 shadow-lg">
+            {Object.entries(pageStrings.philisophies).map(
+              ([key, value], index) => {
+                return (
+                  <div key={index}>
+                    <Accordion title={key} description={value} />
+                  </div>
+                );
+              }
+            )}
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Values
+export default Values;
