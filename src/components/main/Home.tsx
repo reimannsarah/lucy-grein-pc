26769114b48fb671933367fc424262ee9adf2ce4
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-19 20:46:26 -0800

    Add Programs component and update Home component

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -15,12 +15,16 @@ const Home = () => {
         <div className="relative bg-purple py-10 px-14 col-start-4 col-end-8 row-start-1 row-end-2 my-4 z-50 shadow-lg rounded-lg text-4xl">
           {pageStrings.hero.title}
         </div>
-        <div className="bg-purple col-start-5 col-end-9 m-10 flex justify-center items-center rounded-lg border-4 hover:border-coolPurple">
+        <div className="col-start-5 col-end-9 flex justify-center items-center">
           <NavLink to="">
-            <p className="hover:text-coolPurple text-xl">Follow me on Instagram</p>
+            <div className="bg-purple border-4 border-purple hover:border-coolPurple px-10 py-6 rounded-lg hover:text-coolPurple">
+              <p className="text-xl">
+                Follow me on Instagram
+              </p>
+            </div>
           </NavLink>
         </div>
-        <div className="bg-purple py-10 px-14 col-start-5 col-end-9 row-start-2 row-end-4 m-4 shadow-lg rounded-lg border-4 flex items-center">
+        <div className="bg-purple py-10 px-14 col-start-5 col-end-9 row-start-2 row-end-4 m-4 shadow-lg rounded-lg flex items-center">
           <p className="leading-10 text-3xl">{pageStrings.hero.subtitle}</p>
         </div>
       </div>
