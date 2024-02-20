3bfaf1e822bf13f23c3a96b9d36310b17e3ee72e
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-19 20:47:12 -0800

    add journey and valuse content

--- src/assets/text/pageStrings.ts
+++ src/assets/text/pageStrings.ts
@@ -1,33 +1,38 @@
-export const pageStrings = { 
+export const pageStrings = {
   hero: {
     title: "Hi, I'm Lucy",
-    subtitle: "I'm here to make parenting feel accessible, funcitonal, and intuitive"
-
+    subtitle:
+      "I'm here to make parenting feel accessible, funcitonal, and intuitive",
   },
   boxes: {
     box1: {
       top: "Working with families is my passion",
-      bottom: "Learn more about Lucy"
+      bottom: "Learn more about Lucy",
     },
     box2: {
       top: "I believe parents deserve support and collaboration",
-      bottom: "Check out my programs"
+      bottom: "Check out my programs",
     },
     box3: {
       top: "Intuition is a comlex system of values, perception, and memory",
-      bottom: "Read about\n Intuition Theory"
-    }
+      bottom: "Read about\n Intuition Theory",
+    },
   },
   about: {
     introHeader: "Hi there, I'm Lucy",
-    introP1: "I am an emerging parent consultant with a background in behavior management, curriculum development, and trauma informed care. I specialize in early childhood education and working with neurodivergent students. I practice a strengths-based approach to help families build functional systems. As an autistic professional, my career has been shaped by my unique perspective and passion for connection.",
-    introP2: " I am based out of Portland, OR, but have lived in many cities around the world. I spend my time basking in the love of my family and animals, wandering about outdoors, and always listening to, playing, and making music.",
+    introP1:
+      "I am an emerging parent consultant with a background in behavior management, curriculum development, and trauma informed care. I specialize in early childhood education and working with neurodivergent students. I practice a strengths-based approach to help families build functional systems. As an autistic professional, my career has been shaped by my unique perspective and passion for connection.",
+    introP2:
+      " I am based out of Portland, OR, but have lived in many cities around the world. I spend my time basking in the love of my family and animals, wandering about outdoors, and always listening to, playing, and making music.",
     journeyHeader: "My Journey",
     journeyP1: `I have led a winding path that has shaped this ever-evolving career. It all began with my first job as a summer camp counselor for a local nonprofit, My Voice Music (MVM). I received what I believe will be the greatest mentorship of my life over seven summers. I went into my undergraduate program wide-eyed and excited; I studied history and historiography at Macalester College, but ultimately withdrew from the program because I could not connect the dots between my passions and my studies. This led me to World Around Us childcare center where I experienced some of the dysfunctional systems that impact early childhood education. From there, I transitioned to working with families as a private nanny and curriculum designer and fell in love with the intimate connections I fostered.`,
-    journeyP2: "After receiving my autism diagnosis, meandering my way through a global pandemic, and embracing the death of my dear friend and mentor, the founder of MVM, I discovered parent education. I decided to finish my education at Portland State University, where I carved out the path forward as a parenting consultant. This work answers the questions of my past, present, and future; it is what ties me to my community and moves me beyond the realms I already know. Most recently, I am developing Intuition Theory (IT) with my partner, a local curriculum writer and early childhood educator, Grant Peters. I led a workshop on IT at the Oregon Parent Educators Conference in May 2023.",
-    journeyP3: "My career has informed a holistic, trauma-informed, and strengths based approach to working with the foundational units of our communities: families. I have developed an expertise for working with children and specialize in neurodiversity, behavior management, and curriculum development. However, I was called to work with parents when I repeatedly saw the stress and anxiety parents face, especially when trying to navigate their children's disruptive behaviors. I believe that parents deserve support through reflective processing, collaborative tool building, and effective assessment that encourage the development of their own intuitive practices for raising their children.",
-    journeyP4: "The experiences described above have led me to where I am today, working as a parent consultant and developing Intuition Theory. These two projects are deeply aligned. The services I offer are grounded in my knowledge of children and families as well as the philosophical explorations of IT. The theory is situated in the intersections of  behavioral science, psychology, and philosophy. IT describes the phenomenological truths that inform our intuitive responses. I am approaching this development of this model from an experiential standpoint -- I actively use IT to assess the behaviors I observe in children and assist families with defining their personal parenting practices. My goal is to teach parents how to use IT and discover intuitive parenting as an application of the theory.",
-    journeyP5: "I hope you'll join me as the journey continues!" 
+    journeyP2:
+      "After receiving my autism diagnosis, meandering my way through a global pandemic, and embracing the death of my dear friend and mentor, the founder of MVM, I discovered parent education. I decided to finish my education at Portland State University, where I carved out the path forward as a parenting consultant. This work answers the questions of my past, present, and future; it is what ties me to my community and moves me beyond the realms I already know. Most recently, I am developing Intuition Theory (IT) with my partner, a local curriculum writer and early childhood educator, Grant Peters. I led a workshop on IT at the Oregon Parent Educators Conference in May 2023.",
+    journeyP3:
+      "My career has informed a holistic, trauma-informed, and strengths based approach to working with the foundational units of our communities: families. I have developed an expertise for working with children and specialize in neurodiversity, behavior management, and curriculum development. However, I was called to work with parents when I repeatedly saw the stress and anxiety parents face, especially when trying to navigate their children's disruptive behaviors. I believe that parents deserve support through reflective processing, collaborative tool building, and effective assessment that encourage the development of their own intuitive practices for raising their children.",
+    journeyP4:
+      "The experiences described above have led me to where I am today, working as a parent consultant and developing Intuition Theory. These two projects are deeply aligned. The services I offer are grounded in my knowledge of children and families as well as the philosophical explorations of IT. The theory is situated in the intersections of  behavioral science, psychology, and philosophy. IT describes the phenomenological truths that inform our intuitive responses. I am approaching this development of this model from an experiential standpoint -- I actively use IT to assess the behaviors I observe in children and assist families with defining their personal parenting practices. My goal is to teach parents how to use IT and discover intuitive parenting as an application of the theory.",
+    journeyP5: "I hope you'll join me as the journey continues!",
   },
   values: {
     title: "Values and Philosophies",
@@ -35,23 +40,84 @@ export const pageStrings = {
     values_title: "Core Values",
     philisophies_title: "Philisophies of Engagement",
     core_values: {
-      "Community": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-      "Connection": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-      "Creativity": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-      "Resilience": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-      "Comfort": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-      "Responsibility": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-      "Communication": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-      "Self-Reflection": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-      "Accesibility": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
-    }
+      Community:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+      Connection:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+      Creativity:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+      Resilience:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+      Comfort:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+      Responsibility:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+      Communication:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+      "Self-Reflection":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+      Accesibility:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+    },
   },
   philisophies: {
-    "Boundaries create freedom": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-    "Happy, healthy, and safe": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-    "Trial and error": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-    "All experiences are internalized" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-    "Centering the self allows for effective assessment" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+    "Boundaries create freedom":
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+    "Happy, healthy, and safe":
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+    "Trial and error":
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+    "All experiences are internalized":
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+    "Centering the self allows for effective assessment":
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
   },
-  copyright: `\u00A9 Sarah Reimann 2024`,
-}
+  services: {
+    header: "Services Offered",
+    p1: "All of my services, be that individual sessions or multi-month programs, are designed to support your needs. Before we begin working together, we will meet for a free 25-min consultation. I will use that conversation to create an individualized plan for your family that acknowledges its unique dynamic.",
+    plans: {
+      one_on_one: {
+        title: "1:1 PARENT CONSULTING",
+        price: "$90/session, sliding scale available",
+        description: "60-min session, virtual or in-person",
+        headline: "Discover consistent and sustainable parenting practices.",
+        point1: "Assess what's working and what's not working in your home.",
+        point2: "Center yourself, build effective tools",
+        point3: "Develop your parenting intuition",
+      },
+      four_week: {
+        title: "4-WEEK PROBLEM SOLVING PROGRAM",
+        price: "$340 ($85/week)",
+        description: "Four 60-min sessions, virtual or in person",
+        headline:
+          "Let's focus on the issue at hand and prepare for what's next",
+        point1: "Gain problem solving tools to address current struggles",
+        point2:
+          "Practice how to navigate, learn from, and move through your challenges",
+        point3: "Create a problem solving toolkit for future issues",
+      },
+      twelve_week: {
+        title: "12-WEEK INTUITIVE PARENTING PROGRAM",
+        price: "$900 ($75/session), sliding scale available",
+        description:
+          "One 90-min Mutual Understanding session, virtual or in-person",
+        description2: "Eleven 60-min sessions, virtual or in-person",
+        headline: "Discover consistent and sustainable parenting practices.",
+        point1: "Week 1: Mutual Understanding (preparing the land)",
+        point2: "Week 2-4: Situating the Self (tending the soil)",
+        point3: "Week 5-8: Establishing House Culture (planting the seeds)",
+        point4: "Week 9-12: Intuitive Parenting (nurturing the growth)",
+      },
+      share: {
+        title: "SHARE AND RELEASE VENTING SESSION",
+        price: "$35/session",
+        description: "30-min phone call or video chat",
+        headline: "Take up all the space you need.",
+        point1: "Externally process",
+        point2: "Find collaboration",
+        point3: "Brain storm",
+      },
+    },
+    copyright: `\u00A9 Sarah Reimann 2024`,
+  },
+};
