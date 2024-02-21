c75120a5547e19963bc4ca28c4b18b73e6d8a869
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-20 22:36:42 -0800

    change order or elements and remove instagram button from grid

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -6,29 +6,29 @@ import { NavLink } from "react-router-dom";
 const Home = () => {
   return (
     <div className="bg-background my-56 mx-44 p-20 rounded-lg shadow-lg font-body text-2xl">
-      <div className="grid grid-cols-8 grid-rows-5 gap-4">
+      <div className="grid grid-cols-8 grid-rows-3 gap-4">
         <img
           src={imageUrls.hero}
           alt=""
-          className="rounded-lg shadow-lg col-start-1 col-end-5 row-start-1 row-end-6 row-span-5 z-40 border-8 border-pink"
+          className="rounded-lg shadow-lg col-start-1 col-end-5 row-start-1 row-end-6 row-span-5 z-40 border-8 border-purple"
         />
-        <div className="relative bg-purple py-10 px-14 col-start-4 col-end-8 row-start-1 row-end-2 my-4 z-50 shadow-lg rounded-lg text-4xl">
+        <div className="relative bg-purple py-10 px-14 col-start-4 col-end-8 row-start-1 row-end-2 my-4 z-50 shadow-lg rounded-lg text-4xl flex items-center justify-center">
           {pageStrings.hero.title}
-        </div>
-        <div className="col-start-5 col-end-9 flex justify-center items-center">
-          <NavLink to="">
-            <div className="bg-purple border-4 border-purple hover:border-coolPurple px-10 py-6 rounded-lg hover:text-coolPurple">
-              <p className="text-xl">
-                Follow me on Instagram
-              </p>
-            </div>
-          </NavLink>
         </div>
         <div className="bg-purple py-10 px-14 col-start-5 col-end-9 row-start-2 row-end-4 m-4 shadow-lg rounded-lg flex items-center">
           <p className="leading-10 text-3xl">{pageStrings.hero.subtitle}</p>
         </div>
       </div>
-      <InfoBoxes />
+      <div className="my-14">
+        <InfoBoxes />
+      </div>
+      <div className="my-10 mx-auto w-3/5 shadow-lg ">
+        <NavLink to="">
+          <div className="bg-pink border-4 border-pink text-white hover:border-darkblue px-10 py-6 rounded-lg hover:text-darkblue">
+            <p className="text-2xl">Follow me on Instagram</p>
+          </div>
+        </NavLink>
+      </div>
     </div>
   );
 };
