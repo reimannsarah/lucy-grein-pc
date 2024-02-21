5884dea79c56bd45ee17678f8779a17382d64d6b
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-20 22:36:17 -0800

    change color and size of copyright

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -2,9 +2,9 @@ import { pageStrings } from "../../assets/text/pageStrings";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-full text-right p-4">
+    <footer className="relative bottom-0 w-full text-right p-4">
       <div>
-        <p className="text-[#1b3a0695]">{pageStrings.copyright}</p>
+        <p className="text-[#e9f5e135] text-sm">{pageStrings.copyright}</p>
       </div>
     </footer>
   );
