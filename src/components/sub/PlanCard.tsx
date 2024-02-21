8176a64d1750df788f850e249039f777ca28e928
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-20 21:55:05 -0800

    Refactor PlanCard component

--- src/components/sub/PlanCard.tsx
+++ src/components/sub/PlanCard.tsx
@@ -1,4 +1,3 @@
-
 interface PlanProps {
   plan: {
     title: string;
@@ -16,15 +15,20 @@ interface PlanProps {
 const PlanCard = (plan: PlanProps) => {
   return (
     <div className="w-1/2 h-full px-10 text-gray">
-    {/* <h1 className="mb-5 text-3xl text-white bg-pink text-center shadow-lg px-3">{plan.plan.title}</h1> */}
-      <p className="my-3 text-lg">{plan.plan.price}</p>
-      <p className="my-3 text-lg">{plan.plan.description}</p>
-      {plan.plan.description2 && <p className="my-3">{plan.plan.description2}</p>}
-      <strong><p className="text-orange my-5 text-xl text-center">{plan.plan.headline}</p></strong>
+      <strong>
+        <p className="text-orange my-5 text-xl text-center">
+          {plan.plan.headline}
+        </p>
+      </strong>
       <p className="my-3 text-lg">{plan.plan.point1}</p>
       <p className="my-3 text-lg">{plan.plan.point2}</p>
       <p className="my-3 text-lg">{plan.plan.point3}</p>
-      {plan.plan.point4 && <p className="my-3">{plan.plan.point4}</p>}
+      {plan.plan.point4 && <p className="my-3 text-lg">{plan.plan.point4}</p>}
+      <i><p className="mb-3 mt-8 text-md">{plan.plan.price}</p></i>
+      <i><p className="my-3 text-md">{plan.plan.description}</p></i>
+      {plan.plan.description2 && (
+        <p className="my-3 text-md">{plan.plan.description2}</p>
+      )}
     </div>
   );
 };
